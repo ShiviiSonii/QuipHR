82c1c2f7b844b1c14aea2fe84aae772432e0a5f0
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-02-19 14:41:13 +0530

    added form section

---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+class FormComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<div class="form">
+      <p class="form-para">Please fill your information so we can get in touch with you.</p>
+      <div class="form-fields">
+        <div class="form-field">
+            <div class="form-data">
+            <span class="form-span">Name</span>
+            <input type="text" placeholder="Your Name" class="form-input"/>
+            </div>
+            <div class="form-data">
+            <span class="form-span">Name</span>
+            <input type="text" placeholder="Your Name" class="form-input"/>
+            </div>
+        </div>
+        <div class="form-field">
+            <div class="form-data">
+            <span class="form-span">Name</span>
+            <input type="text" placeholder="Your Name" class="form-input"/>
+            </div>
+            <div class="form-data">
+            <span class="form-span">Name</span>
+            <input type="text" placeholder="Your Name" class="form-input"/>
+            </div>
+        </div>
+      </div>
+      <button class="form-btn">Next</button>
+      </div>`;
+  }
+}
+
+customElements.define("form-component", FormComponent);
