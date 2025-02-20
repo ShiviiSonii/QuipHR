9852cd6428e43db1fd43716b834ee7d5c99eed4c
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-02-20 13:25:24 +0530

    testimonial section fixed

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -6,8 +6,7 @@ class TestimonialComponent extends HTMLElement {
             <div class="testimonial-slide slide-prev">
               <div class="testimonial-card">
                 <div class="testimonial-content">
-                  <div class="testimonial-avatar"></div>
-                  <p class="testimonial-text">"The service exceeded my expectations. The team was professional and delivered outstanding results."</p>
+                 <p class="testimonial-text">"The service exceeded my expectations. The team was professional and delivered outstanding results."</p>
                   <p class="testimonial-author">John Smith</p>
                   <p class="testimonial-position">CEO, Tech Corp</p>
                 </div>
@@ -16,8 +15,7 @@ class TestimonialComponent extends HTMLElement {
             <div class="testimonial-slide slide-current">
               <div class="testimonial-card">
                 <div class="testimonial-content">
-                  <div class="testimonial-avatar"></div>
-                  <p class="testimonial-text">"I couldn't be happier with the outcome. The attention to detail and dedication to quality was impressive."</p>
+                 <p class="testimonial-text">"I couldn't be happier with the outcome. The attention to detail and dedication to quality was impressive."</p>
                   <p class="testimonial-author">Sarah Johnson</p>
                   <p class="testimonial-position">Marketing Director</p>
                 </div>
@@ -26,7 +24,6 @@ class TestimonialComponent extends HTMLElement {
             <div class="testimonial-slide slide-next">
               <div class="testimonial-card">
                 <div class="testimonial-content">
-                  <div class="testimonial-avatar"></div>
                   <p class="testimonial-text">"Working with this team has been a game-changer for our business. Highly recommended!"</p>
                   <p class="testimonial-author">Mike Wilson</p>
                   <p class="testimonial-position">Product Manager</p>
