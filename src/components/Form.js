8ca70dff28d405c685fdc9b9dc5b7671c80280c5
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-02-21 11:51:20 +0530

    form validation added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,31 @@
 class FormComponent extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<div class="form">
+    this.innerHTML = `<form class="form">
       <p class="form-para">Please fill your information so we can get in touch with you.</p>
       <div class="form-fields">
         <div class="form-field">
-            <div class="form-data">
+          <div class="form-data">
             <span class="form-span">Name</span>
-            <input type="text" placeholder="Your Name" class="form-input"/>
-            </div>
-            <div class="form-data">
-            <span class="form-span">Name</span>
-            <input type="text" placeholder="Your Name" class="form-input"/>
-            </div>
+            <input type="text" placeholder="Your Name" class="form-input" required/>
+          </div>
+          <div class="form-data">
+            <span class="form-span">Email</span>
+            <input type="email" placeholder="Your Email" class="form-input" required/>
+          </div>
         </div>
         <div class="form-field">
-            <div class="form-data">
-            <span class="form-span">Name</span>
-            <input type="text" placeholder="Your Name" class="form-input"/>
-            </div>
-            <div class="form-data">
-            <span class="form-span">Name</span>
-            <input type="text" placeholder="Your Name" class="form-input"/>
-            </div>
+          <div class="form-data">
+            <span class="form-span">Phone</span>
+            <input type="tel" placeholder="Your Phone" class="form-input" required/>
+          </div>
+          <div class="form-data">
+            <span class="form-span">Message</span>
+            <input type="text" placeholder="Your message" class="form-input" required/>
+          </div>
         </div>
       </div>
-      <button class="form-btn">Submit</button>
-      </div>`;
+      <button type="submit" class="form-btn">Submit</button>
+    </form>`;
   }
 }
 
